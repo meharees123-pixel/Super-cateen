26f562f5adafe6bf967bbf80cd36737bc9f64086
Meharees <mehareesayur@gmail.com>
2026-03-19 16:54:02 +0300

    change route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'orders/success/:id',
+    renderMode: RenderMode.Ssr
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
